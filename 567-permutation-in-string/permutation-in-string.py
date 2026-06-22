3dc203dae97d0a2326a40d561a355ab5921c90d3
Avirat Pal <aviratpal2004@gmail.com>
2026-06-23 03:35:10 +0530

    Time: 19 ms (90.60%) | Memory: 12.8 MB (45.21%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,13 +1,36 @@
 class Solution(object):
     def checkInclusion(self, s1, s2):
-        if s1 in s2:
-            return True
-        a=sorted(s1)
-        k=len(a)
-        for i in range(0,len(s2)-k+1):
-            if sorted(s2[i:i+k])==a:
-                return True
-        return False        
+        if len(s1)>len(s2):
+            return False
+        frequency={}
+        for i in s1:
+            if i in frequency:
+                frequency[i]+=1
+            else:
+                frequency[i]=1
+        k=len(s1)
+        window={}
+        for i in range(k):
+            if s2[i] in window:
+                window[s2[i]]+=1
+            else:
+                window[s2[i]]=1
+        if frequency==window:
+            return True   
+        for i in range(k,len(s2)):
+            if s2[i] in window:
+                window[s2[i]]+=1
+            else:
+                window[s2[i]]=1
+            old_char=s2[i-k]
+            window[old_char]-=1
+            if window[old_char]==0:
+                del window[old_char]
+            if window==frequency:
+                return True    
+        return False                
+
+
 
 
         
