70ae8e36e5868f5238f730187fcdc6d4cc40c662
Avirat Pal <aviratpal2004@gmail.com>
2026-07-02 13:27:35 +0530

    Time: 129 ms (58.19%) | Memory: 66.5 MB (59.00%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isPalindrome(self, head):
+        slow=head
+        fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        previous=None
+        current=slow
+        while current:
+            next_node=current.next
+            current.next=previous
+            previous=current
+            current=next_node
+        left=head
+        right=previous
+        while right:
+            if left.val!=right.val:
+                return False
+            left=left.next
+            right=right.next
+        return True            
+
+       
+
+        
+               
+
+     
