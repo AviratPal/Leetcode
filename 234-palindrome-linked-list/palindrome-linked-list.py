7f91be2b6b3a1c2a6a4c1e7915d043463171ff13
Avirat Pal <aviratpal2004@gmail.com>
2026-07-11 12:40:39 +0530

    Time: 122 ms (81.77%) | Memory: 66.3 MB (71.12%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,27 +1,27 @@
 class Solution(object):
     def isPalindrome(self, head):
-        slow=head
-        fast=head
+        slow = head
+        fast = head
         while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
-        previous=None
-        current=slow
+            slow = slow.next
+            fast = fast.next.next
+        if fast:
+            slow = slow.next
+        previous = None
+        current = slow
         while current:
-            next_node=current.next
-            current.next=previous
-            previous=current
-            current=next_node
-        left=head
-        right=previous
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+        left = head
+        right = previous
         while right:
-            if left.val!=right.val:
+            if left.val != right.val:
                 return False
-            left=left.next
-            right=right.next
-        return True            
-
-       
+            left = left.next
+            right = right.next
+        return True
 
         
                
