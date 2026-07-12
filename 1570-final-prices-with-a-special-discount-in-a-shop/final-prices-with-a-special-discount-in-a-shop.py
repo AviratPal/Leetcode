b429c295e8121b5c6f12d97fdc657e7585cdcbfb
Avirat Pal <aviratpal2004@gmail.com>
2026-07-12 18:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (12.14%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def finalPrices(self, prices):
+        stack=[]
+        ans=[0]*len(prices)
+        for i in range(len(prices)-1,-1,-1):
+            while stack and stack[-1]>prices[i]:
+                stack.pop()
+            if stack:
+                ans[i]=prices[i]-stack[-1]
+            else:
+                ans[i]=prices[i]
+            stack.append(prices[i])  
+        return ans              
+
+     
